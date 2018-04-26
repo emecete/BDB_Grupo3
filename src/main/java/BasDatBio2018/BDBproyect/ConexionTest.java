63fcea657ebcbe13c81898b83500d4a646de0e9b
javimoreno18 <javimoreno1806@uma.es>
2018-04-26 18:21:15 +0200

    Correction of the ConexionText implementation. Code added in order to do queries

--- src/main/java/BasDatBio2018/BDBproyect/ConexionTest.java
+++ src/main/java/BasDatBio2018/BDBproyect/ConexionTest.java
@@ -3,6 +3,10 @@ package BasDatBio2018.BDBproyect;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.ResultSet;
 
 public class ConexionTest {
 	
@@ -14,15 +18,26 @@ public class ConexionTest {
 
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
         } catch (Exception ex) {
-            // handle the error
+        	System.out.println(ex.getMessage());
         }
         Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        String query = "SELECT gene_id from gene";
+        
         try {
-            conn =
+            
+        	conn =
                DriverManager.getConnection("jdbc:mysql://localhost:3306/ailuropoda_melanoleuca_core_91_1?" +
-                                           "user=root&password=mojitomoreno96&useSSL=false");
+                                           "user=root&password=mojitomoreno96&useSSL=false&serverTimezone=UTC");
 
             // Do something with the Connection
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(query);
+            
+            if (stmt.execute(query)) {
+                rs = stmt.getResultSet();
+            }
 
            
         } catch (SQLException ex) {
@@ -30,6 +45,23 @@ public class ConexionTest {
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
+        }
+        finally {
+        	if(rs != null) {
+        		try {
+        			rs.close();
+        		}catch (SQLException sqlEx) {} //ignore
+        		
+        		rs = null;
+        	}
+        	
+        	if (stmt != null) {
+        		try {
+        			stmt.close();
+        		} catch (SQLException sqlEx) {} //ignore
+        		
+        		stmt = null;
+        	}
         }
     }
 }
